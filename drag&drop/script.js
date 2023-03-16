1e46b2c28f49b77c1b1a73a0ee49218b88d9e7a2
Ozan <ozancangu24@gmail.com>
2023-03-16 23:24:44 +0300

    script.js created

--- drag&drop/script.js
+++ drag&drop/script.js
@@ -1,111 +1,204 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Drag and Drop</title>
-    <link rel="icon" type="image/png" href="favicon.png">
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <h1 class="main-title">Kanban Board</h1>
-    <div class="drag-container">
-        <ul class="drag-list">
-            <!-- Backlog Column -->
-            <li class="drag-column backlog-column">
-                <span class="header">
-                    <h1>Backlog</h1>
-                </span>
-                <!-- Backlog Content -->
-                <div id="backlog-content" class="custom-scroll">
-                    <ul class="drag-item-list" id="backlog-list" ondrop="drop(event)" ondragover="allowDrop(event)" ondragenter="dragEnter(0)"></ul>
-                </div>
-                <!-- Add Button Group -->
-                <div class="add-btn-group">
-                    <div class="add-btn" onclick="showInputBox(0)">
-                        <span class="plus-sign">+</span>
-                        <span>Add Item</span>
-                    </div>
-                    <div class="add-btn solid" onclick="hideInputBox(0)">
-                        <span>Save Item</span>
-                    </div>
-                </div>
-                <div class="add-container">
-                    <div class="add-item" contenteditable="true"></div>
-                </div>
-            </li>
-            <!-- Progress Column -->
-            <li class="drag-column progress-column">
-                <span class="header">
-                    <h1>In Progress</h1>
-                </span>
-                <!-- Progress Content -->
-                <div id="progress-content" class="custom-scroll">
-                    <ul class="drag-item-list" id="progress-list" ondrop="drop(event)" ondragover="allowDrop(event)" ondragenter="dragEnter(1)"></ul>
-                </div>
-                <!-- Add Button Group -->
-                <div class="add-btn-group">
-                    <div class="add-btn" onclick="showInputBox(1)">
-                        <span class="plus-sign">+</span>
-                        <span>Add Item</span>
-                    </div>
-                    <div class="add-btn solid" onclick="hideInputBox(1)">
-                        <span>Save Item</span>
-                    </div>
-                </div>
-                <div class="add-container">
-                    <div class="add-item" contenteditable="true"></div>
-                </div>
-            </li>
-            <!-- Complete Column -->
-            <li class="drag-column complete-column">
-                <span class="header">
-                    <h1>Complete</h1>
-                </span>
-                <!-- Complete Content -->
-                <div id="complete-content" class="custom-scroll">
-                    <ul class="drag-item-list" id="complete-list" ondrop="drop(event)" ondragover="allowDrop(event)" ondragenter="dragEnter(2)"></ul>
-                </div>
-                <!-- Add Button Group -->
-                <div class="add-btn-group">
-                    <div class="add-btn" onclick="showInputBox(2)">
-                        <span class="plus-sign">+</span>
-                        <span>Add Item</span>
-                    </div>
-                    <div class="add-btn solid" onclick="hideInputBox(2)">
-                        <span>Save Item</span>
-                    </div>
-                </div>
-                <div class="add-container">
-                    <div class="add-item" contenteditable="true"></div>
-                </div>
-            </li>
-            <!-- On Hold Column -->
-            <li class="drag-column on-hold-column">
-                <span class="header">
-                    <h1>On Hold</h1>
-                </span>
-                <!-- On Hold Content -->
-                <div id="on-hold-content" class="custom-scroll">
-                    <ul class="drag-item-list" id="on-hold-list" ondrop="drop(event)" ondragover="allowDrop(event)" ondragenter="dragEnter(3)"></ul>
-                </div>
-                <!-- Add Button Group -->
-                <div class="add-btn-group">
-                    <div class="add-btn" onclick="showInputBox(3)">
-                        <span class="plus-sign">+</span>
-                        <span>Add Item</span>
-                    </div>
-                    <div class="add-btn solid" onclick="hideInputBox(3)">
-                        <span>Save Item</span>
-                    </div>
-                </div>
-                <div class="add-container">
-                    <div class="add-item" contenteditable="true"></div>
-                </div>
-            </li>
-        </ul>
-    </div>
-    <!-- Script -->
-    <script src="script.js"></script>
-</body>
-</html>
+
+
+
+
+
+const addBtns = document.querySelectorAll('.add-btn:not(.solid)');
+const saveItemBtns = document.querySelectorAll('.solid');
+const addItemContainers = document.querySelectorAll('.add-container');
+const addItems = document.querySelectorAll('.add-item');
+// Item Lists
+const listColumns = document.querySelectorAll('.drag-item-list');
+const backlogListEl = document.getElementById('backlog-list');
+const progressListEl = document.getElementById('progress-list');
+const completeListEl = document.getElementById('complete-list');
+const onHoldListEl = document.getElementById('on-hold-list');
+
+// Items
+let updatedOnLoad = false;
+
+// Initialize Arrays
+let backlogListArray = [];
+let progressListArray = [];
+let completeListArray = [];
+let onHoldListArray = [];
+let listArrays = [];
+
+// Drag Functionality
+let draggedItem;
+let dragging = false;
+let currentColumn;
+
+// Get Arrays from localStorage if available, set default values if not
+function getSavedColumns() {
+  if (localStorage.getItem('backlogItems')) {
+    backlogListArray = JSON.parse(localStorage.backlogItems);
+    progressListArray = JSON.parse(localStorage.progressItems);
+    completeListArray = JSON.parse(localStorage.completeItems);
+    onHoldListArray = JSON.parse(localStorage.onHoldItems);
+  } else {
+    backlogListArray = ['Release the course', 'Sit back and relax'];
+    progressListArray = ['Work on projects', 'Listen to music'];
+    completeListArray = ['Being cool', 'Getting stuff done'];
+    onHoldListArray = ['Being uncool'];
+  }
+}
+
+// Set localStorage Arrays
+function updateSavedColumns() {
+  listArrays = [backlogListArray, progressListArray, completeListArray, onHoldListArray];
+  const arrayNames = ['backlog', 'progress', 'complete', 'onHold'];
+  arrayNames.forEach((arrayName, index) => {
+    localStorage.setItem(`${arrayName}Items`, JSON.stringify(listArrays[index]));
+  });
+}
+
+// Filter Array to remove empty values
+function filterArray(array) {
+  const filteredArray = array.filter(item => item !== null);
+  return filteredArray;
+}
+
+// Create DOM Elements for each list item
+function createItemEl(columnEl, column, item, index) {
+  // List Item
+  const listEl = document.createElement('li');
+  listEl.textContent = item;
+  listEl.id = index;
+  listEl.classList.add('drag-item');
+  listEl.draggable = true;
+  listEl.setAttribute('onfocusout', `updateItem(${index}, ${column})`);
+  listEl.setAttribute('ondragstart', 'drag(event)');
+  listEl.contentEditable = true;
+  // Append
+  columnEl.appendChild(listEl);
+}
+
+// Update Columns in DOM - Reset HTML, Filter Array, Update localStorage
+function updateDOM() {
+  // Check localStorage once
+  if (!updatedOnLoad) {
+    getSavedColumns();
+  }
+  // Backlog Column
+  backlogListEl.textContent = '';
+  backlogListArray.forEach((backlogItem, index) => {
+    createItemEl(backlogListEl, 0, backlogItem, index);
+  });
+  backlogListArray = filterArray(backlogListArray);
+  // Progress Column
+  progressListEl.textContent = '';
+  progressListArray.forEach((progressItem, index) => {
+    createItemEl(progressListEl, 1, progressItem, index);
+  });
+  progressListArray = filterArray(progressListArray);
+  // Complete Column
+  completeListEl.textContent = '';
+  completeListArray.forEach((completeItem, index) => {
+    createItemEl(completeListEl, 2, completeItem, index);
+  });
+  completeListArray = filterArray(completeListArray);
+  // On Hold Column
+  onHoldListEl.textContent = '';
+  onHoldListArray.forEach((onHoldItem, index) => {
+    createItemEl(onHoldListEl, 3, onHoldItem, index);
+  });
+  onHoldListArray = filterArray(onHoldListArray);
+  // Don't run more than once, Update Local Storage
+  updatedOnLoad = true;
+  updateSavedColumns();
+}
+
+// Update Item - Delete if necessary, or update Array value
+function updateItem(id, column) {
+  const selectedArray = listArrays[column];
+  const selectedColumn = listColumns[column].children;
+  if (!dragging) {
+    if (!selectedColumn[id].textContent) {
+      delete selectedArray[id];
+    } else {
+      selectedArray[id] = selectedColumn[id].textContent;
+    }
+    updateDOM();
+  }
+}
+
+// Add to Column List, Reset Textbox
+function addToColumn(column) {
+  const itemText = addItems[column].textContent;
+  const selectedArray = listArrays[column];
+  selectedArray.push(itemText);
+  addItems[column].textContent = '';
+  updateDOM(column);
+}
+
+// Show Add Item Input Box
+function showInputBox(column) {
+  addBtns[column].style.visibility = 'hidden';
+  saveItemBtns[column].style.display = 'flex';
+  addItemContainers[column].style.display = 'flex';
+}
+
+// Hide Item Input Box
+function hideInputBox(column) {
+  addBtns[column].style.visibility = 'visible';
+  saveItemBtns[column].style.display = 'none';
+  addItemContainers[column].style.display = 'none';
+  addToColumn(column);
+}
+
+// Allows arrays to reflect Drag and Drop items
+function rebuildArrays() {
+  backlogListArray = [];
+  for (let i = 0; i < backlogListEl.children.length; i++) {
+    backlogListArray.push(backlogListEl.children[i].textContent);
+  }
+  progressListArray = [];
+  for (let i = 0; i < progressListEl.children.length; i++) {
+    progressListArray.push(progressListEl.children[i].textContent);
+  }
+  completeListArray = [];
+  for (let i = 0; i < completeListEl.children.length; i++) {
+    completeListArray.push(completeListEl.children[i].textContent);
+  }
+  onHoldListArray = [];
+  for (let i = 0; i < onHoldListEl.children.length; i++) {
+    onHoldListArray.push(onHoldListEl.children[i].textContent);
+  }
+  updateDOM();
+}
+
+// When Item Enters Column Area
+function dragEnter(column) {
+  listColumns[column].classList.add('over');
+  currentColumn = column;
+}
+
+// When Item Starts Dragging
+function drag(e) {
+  draggedItem = e.target;
+  dragging = true;
+}
+
+// Column Allows for Item to Drop
+function allowDrop(e) {
+  e.preventDefault();
+}
+
+// Dropping Item in Column
+function drop(e) {
+  e.preventDefault();
+  const parent = listColumns[currentColumn];
+  // Remove Background Color/Padding
+  listColumns.forEach((column) => {
+    column.classList.remove('over');
+  });
+  // Add item to Column
+  parent.appendChild(draggedItem);
+  // Dragging complete
+  dragging = false;
+  rebuildArrays();
+}
+
+// On Load
+updateDOM();
